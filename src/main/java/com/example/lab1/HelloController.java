bd27db3246d8130dd64f0948ae974b87d703f5ff
Mark Veerasingam <G00403931@atu.ie>
2023-09-27 15:43:27 +0100

    added request param for name and age

--- src/main/java/com/example/lab1/HelloController.java
+++ src/main/java/com/example/lab1/HelloController.java
@@ -2,6 +2,7 @@ package com.example.lab1;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,5 +18,9 @@ public class HelloController {
         return "Hello, " + name;
     }
 
-
+    @GetMapping("/details")
+    public String greetByName(@RequestParam String name,
+                              @RequestParam int age) {
+        return "Welcome, " + name + " Your age is " + age;
+    }
 }
