f6a38008e757b3e2fa278bcc08a6539f7b817044
Mark Veerasingam <markveer70@gmail.com>
2023-09-27 21:25:35 +0100

    developed a basic calculator w @RequestParam

--- src/main/java/com/example/lab1/CalculatorController.java
+++ src/main/java/com/example/lab1/CalculatorController.java
@@ -1,4 +1,37 @@
 package com.example.lab1;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CalculatorController {
+    @GetMapping("/calculate")
+    public String calculate(@RequestParam int num1,
+                         @RequestParam int num2,
+                         @RequestParam String operation){
+        int total = 0;
+        String symbol = " + ";
+
+        switch (operation) {
+            case "add":
+                total = num1 + num2;
+                break;
+            case "subtract":
+                total = num1 - num2;
+                symbol = " - ";
+                break;
+            case "multiply":
+                total = num1 * num2;
+                symbol = " * ";
+                break;
+            case "divide":
+                total = num1 / num2;
+                symbol = " / ";
+                break;
+            default:
+                System.out.println("Invalid operation: " + operation);
+        }
+        return num1 + symbol + num2 + " = " + total;
+    }
 }
