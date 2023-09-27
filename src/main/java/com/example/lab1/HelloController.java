260d9085e520c4e007d79cdfc45638c3b2fbc903
Mark Veerasingam <G00403931@atu.ie>
2023-09-27 15:28:22 +0100

    added @PathVariable for method greetByName

--- src/main/java/com/example/lab1/HelloController.java
+++ src/main/java/com/example/lab1/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.lab1;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +10,11 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(){
         return "Hello, World";
+    }
+
+    @GetMapping("/greet/{name}")
+    public String greetByName(@PathVariable String name) {
+        return "Hello, " + name;
     }
 
 }
