98e09d3422682c4b29b6073587b92c9ff795fcce
Mark Veerasingam <markveer70@gmail.com>
2023-09-29 19:03:29 +0100

    updated CalculatorReturnTotal getter and setter

--- src/main/java/ie/atu/lab1/CalculatorReturnTotal.java
+++ src/main/java/ie/atu/lab1/CalculatorReturnTotal.java
@@ -1,12 +1,15 @@
 package ie.atu.lab1;
 
+// Note: if the return type from an endpoint is an object, the response is automatically converted to
+//JSON format by Spring Boot's built-in support for content negotiation and serialization
+
 // make a class to store information about what operation you'll use and to return the total
 // this code returns an int(total) and string(operation)
 // ref: https://stackoverflow.com/questions/10328604/return-a-string-and-int
 
 public class CalculatorReturnTotal {
-    private String operation;
-    private int total;
+    private final String operation;
+    private final int total;
 
     //constructor
     public CalculatorReturnTotal(String operation, int total) {
@@ -14,20 +17,8 @@ public class CalculatorReturnTotal {
         this.total = total;
     }
 
-    // getters + setters
-    public String getOperation() {
-        return operation;
-    }
+    // getters and setters
+    public String getOperation() {return operation;}
+    public int getTotal() {return total;}
 
-    public void setOperation(String operation) {
-        this.operation = operation;
-    }
-
-    public int getTotal() {
-        return total;
-    }
-
-    public void setTotal(int total) {
-        this.total = total;
-    }
 }
